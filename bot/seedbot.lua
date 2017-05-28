ff74e23569477fb15d737ff3a56990982532ff4b
shayannamayande <shayan.na80@gmail.com>
2017-05-28 13:11:51 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "set",
     "get",
     "broadcast",
-    sudo_users={shayannamayande},--sudo users
+    sudo_users={110626080},--sudo users
     "invite",
     "all",
     "leave_ban",
